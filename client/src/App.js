91caecf3289dc112713458fc0ed06a611e53c6b1
LexTheMenace <lexthemenace@gmail.com>
2020-10-06 09:02:46 -0400

    bootswatch and router installed, app cleanup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import "bootswatch/dist/slate/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> HEy </h1>
+      <p> Paragraph </p>
+    </div>
+  );
+}
+
+export default App;
