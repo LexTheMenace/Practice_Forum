af724c60bd88d02a51f654065664ca0d6701c692
LexTheMenace <lexthemenace@gmail.com>
2020-10-06 09:52:21 -0400

    err handling added to serverjs

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const { notFound, errorHandler } = require('./middleware')
 
 var app = express();
 
@@ -9,5 +10,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Routes
+
+// Error Handling
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
