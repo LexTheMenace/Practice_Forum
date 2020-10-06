31e383da43cbbcc088c442140175be3f1f4f33c8
LexTheMenace <lexthemenace@gmail.com>
2020-10-06 09:19:49 -0400

    welcome jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import "bootswatch/dist/slate/bootstrap.css";
-import AppNav from './components/AppNav';
+import AppNav from './components/layout/AppNav';
+import Jumbotron from './components/layout/Jumbotron';
 function App() {
   return (
-    <div className="App">
+    <div>
       <AppNav/>
+         <div className="container">
+      
+      <Jumbotron/>
     </div>
+    </div>
+ 
   );
 }
 
---
+++ client/src/components/layout/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Jumbotron() {
+    return (
+        <div class="jumbotron">
+        <h1 class="display-3">Howdy Y'all!</h1>
+        <p class="lead">Welcome to Kia's Corner! </p>
+        <hr class="my-4"/>
+        <p>We all on a journey to improve ourselves, come kick it with me!</p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
+          <a class="btn btn-primary btn-lg" href="#" role="button">Sign In</a>
+
+        </p>
+      </div>
+    )
+}
