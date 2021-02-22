4cab8e6d26c20b2f338ef572c6c786af0b92b10b
LexTheMenace <lexthemenace@gmail.com>
2021-02-22 09:42:51 -0500

    remove joi

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,4 @@
 const db = require('../models');
-const Joi = require('joi');
-
-const schema = Joi.object().keys({
-  display_name: Joi.string().required(),
-  email: Joi.string().email(),
-  google_id: Joi.string().required(),
-  image_url: Joi.string().uri({
-    scheme: [
-      /https/
-    ]
-  }),
-  role_id: Joi.number().integer()
-});
-
 
 module.exports = {
   findByEmail: (email) => {
